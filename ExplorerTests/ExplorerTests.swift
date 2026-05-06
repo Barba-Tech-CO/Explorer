e0d9ab5da27c888506d68d3cfed7e0b7e1c56d5b
Barba2k2 <lorenzo.dz@hotmail.com>
2026-05-06 09:56:13 -0300

    Initial Commit

---
+++ ExplorerTests/ExplorerTests.swift
@@ -0,0 +1,19 @@
+//
+//  ExplorerTests.swift
+//  ExplorerTests
+//
+//  Created by Barba Dz on 5/6/26.
+//
+
+import Testing
+@testable import Explorer
+
+struct ExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
